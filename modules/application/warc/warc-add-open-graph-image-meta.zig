cd3b66b17340139ecd99cdcf90b3392cb2a06fe4
Patrick Smith <pgwsmith@gmail.com>
2026-05-12 00:08:30 +0100

    Increase input/output for adding open graph image meta to warc

--- modules/application/warc/warc-add-open-graph-image-meta.zig
+++ modules/application/warc/warc-add-open-graph-image-meta.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
-const INPUT_CAP: usize = 64 * 1024 * 1024;
-const OUTPUT_CAP: usize = 64 * 1024 * 1024;
+const INPUT_CAP: usize = 256 * 1024 * 1024;
+const OUTPUT_CAP: usize = 256 * 1024 * 1024;
 const INPUT_CONTENT_TYPE = "application/warc";
 const OUTPUT_CONTENT_TYPE = "application/warc";
 
